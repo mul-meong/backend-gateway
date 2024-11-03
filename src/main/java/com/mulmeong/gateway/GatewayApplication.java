2dfe564c51ee4e8c06155d7b9df9551832780cad
SeongGwang <seonggwangdev@gmail.com>
2024-11-03 19:17:20 +0900

    ✨ feat: cors 설정 및 client 활성화

--- src/main/java/com/mulmeong/gateway/GatewayApplication.java
+++ src/main/java/com/mulmeong/gateway/GatewayApplication.java
@@ -2,7 +2,9 @@ package com.mulmeong.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
 

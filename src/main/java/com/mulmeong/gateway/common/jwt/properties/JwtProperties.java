4d12377a1ccd09b810a7b4b1b34094faa3621bf0
SeongGwang <seonggwangdev@gmail.com>
2024-11-05 10:25:44 +0900

    🐛 fix: JwtProperties에 @ConfigurationPropertiesScan 사용
     Properties 파일의 Setter 삭제

--- src/main/java/com/mulmeong/gateway/common/jwt/properties/JwtProperties.java
+++ src/main/java/com/mulmeong/gateway/common/jwt/properties/JwtProperties.java
@@ -1,13 +1,11 @@
 package com.mulmeong.gateway.common.jwt.properties;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
 
 @Getter
-@Setter
-@Component
+@ConfigurationPropertiesBinding
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
     //자바 클래스에 jwt 프로퍼티값을 가져와 사용할 수 있게 함
@@ -20,5 +18,15 @@ public class JwtProperties {
     private String accessTokenPrefix;
     private String refreshTokenPrefix;
 
+    public JwtProperties(String issuer, String secretKey, long accessExpireTime, long refreshExpireTime,
+                         String tokenPrefix, String accessTokenPrefix, String refreshTokenPrefix) {
+        this.issuer = issuer;
+        this.secretKey = secretKey;
+        this.accessExpireTime = accessExpireTime;
+        this.refreshExpireTime = refreshExpireTime;
+        this.tokenPrefix = tokenPrefix;
+        this.accessTokenPrefix = accessTokenPrefix;
+        this.refreshTokenPrefix = refreshTokenPrefix;
+    }
 }
 
